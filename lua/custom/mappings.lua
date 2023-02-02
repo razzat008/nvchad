9ac642ab4d5f5877b8842fd47d4c11d54cd689f1
ceaser008 <radiusrazzat111@gmail.com>
2023-02-02 12:22:07 +0545

    custom mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,6 +3,12 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    --close buffer
+    ["<leader>x"] = { ":bufdo close", "close buffer" },
+
+    --split window
+    ["<leader>sv"] = { "<C-w>v", "split vertically" },
   },
 }
 
