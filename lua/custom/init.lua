84b72afde73ba579fb1330f271d46fd7e40b7d45
ceaser008 <radiusrazzat111@gmail.com>
2023-03-03 05:10:36 +0545

    relative number

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,5 +1,12 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+local opt = vim.opt
+local g = vim.g
+
+-- for numbers
+opt.relativenumber = true
+
+
 -- Auto resize panes when resizing nvim window
 autocmd("VimResized", {
   pattern = "*",
