ff34cc6cc714aef34974a453422cfbc9b14e338d
ceaser008 <radiusrazzat111@gmail.com>
2023-03-16 06:11:36 +0545

    theme

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme_toggle = { "gatekeeper", "decay" },
-  theme = "gruvbox",
+  theme = "tokyodark",
   hl_override = highlights.override,
   hl_add = highlights.add,
 
