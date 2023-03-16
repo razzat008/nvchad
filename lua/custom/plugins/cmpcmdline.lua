aff3d4a6490ee3ef2d84876f70d64217770c54e2
ceaser008 <radiusrazzat111@gmail.com>
2023-03-16 06:12:50 +0545

    cmdline plugin config

---
+++ lua/custom/plugins/cmpcmdline.lua
@@ -0,0 +1,20 @@
+local present, cmdline = pcall(require, "cmdline")
+
+if not present then
+  return
+end
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    {
+      name = "cmdline",
+      option = {
+        ignore_cmds = { "Man", "!" },
+      },
+    },
+  }),
+})
