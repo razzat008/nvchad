359a109a99a3e96b8eb6ecb8337a3d9a246350a6
ceaser008 <radiusrazzat111@gmail.com>
2023-04-12 09:18:24 +0545

    formatting

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,82 +1,91 @@
-local overrides = require("custom.configs.overrides")
+local overrides = require "custom.configs.overrides"
 
 local plugins = {
 
-	-- Override plugin definition options
+  -- Override plugin definition options
 
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			-- format & linting
-			{
-				"jose-elias-alvarez/null-ls.nvim",
-				config = function()
-					require("custom.configs.null-ls")
-				end,
-			},
-		},
-		config = function()
-			require("plugins.configs.lspconfig")
-			require("custom.configs.lspconfig")
-		end, -- Override to setup mason-lspconfig
-	},
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
 
-	-- override plugin configs
-	{
-		"williamboman/mason.nvim",
-		opts = overrides.mason,
-	},
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
+  },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = overrides.treesitter,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		opts = overrides.nvimtree,
-	},
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
 
-	-- Install a plugin
-	-- {
-	-- 	"max397574/better-escape.nvim",
-	-- 	event = "InsertEnter",
-	-- 	config = function()
-	-- 		require("better_escape").setup()
-	-- 	end,
-	-- },
+  -- Install a plugin
+  -- {
+  -- 	"max397574/better-escape.nvim",
+  -- 	event = "InsertEnter",
+  -- 	config = function()
+  -- 		require("better_escape").setup()
+  -- 	end,
+  -- },
 
-	-- To make a plugin not be loaded
-	-- {
-	--   "NvChad/nvim-colorizer.lua",
-	--   enabled = false
-	-- },
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
 
-	-- Uncomment if you want to re-enable which-key
-	{
-		"folke/which-key.nvim",
-		enabled = false,
-	},
+  -- Uncomment if you want to re-enable which-key
+  {
+    "folke/which-key.nvim",
+    enabled = false,
+  },
 
-	--markdown
-	{
-		"toppair/peek.nvim",
-		ft = { "markdown" },
-		build = "deno task --quiet build:fast",
-		config = function()
-			require("custom.configs.peek")
-		end,
-	},
+  --markdown
+  {
+    "toppair/peek.nvim",
+    ft = { "markdown" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require "custom.configs.peek"
+    end,
+  },
 
-	-- easy navigation
-	{
-		"ggandor/lightspeed.nvim",
-		lazy = true,
-    enabled=false,
-		config = function()
-			require("custom.configs.lightspeed")
-		end,
-	},
+  -- easy navigation
+  {
+    "ggandor/lightspeed.nvim",
+    enabled = false,
+    config = function()
+      require "custom.configs.lightspeed"
+    end,
+  },
+
+  -- harpoon
+
+  {
+    "ThePrimeagen/harpoon",
+    event = "BufEnter",
+    config = function()
+      require "custom.configs.harpoon"
+    end,
+  },
 }
 
 return plugins
