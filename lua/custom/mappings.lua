e1606971aeeb47abec74a0e79fe3dece414ae7f1
ceaser008 <radiusrazzat111@gmail.com>
2023-04-15 10:46:30 +0545

    removed harpoon's mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,6 +1,4 @@
 local M = {}
---local mark = require("harpoon.mark")
---local ui = require("harpoon.ui")
 
 M.general = {
   n = {
@@ -10,7 +8,6 @@ M.general = {
 
     --close buffer
     -- ["<leader>x"] = { ":bufdo close<cr>", "close buffer" },
-    -- ["<C-w>"] = { ":close<cr>", "close buffer" },
     -- basic stuffs from theprimegen
     ["<C-u>"] = { "<C-u>zz", "move half page up" },
     ["<C-d>"] = { "<C-d>zz", "move half page down" },
@@ -23,8 +20,6 @@ M.general = {
 
     --split window
     ["<leader>sv"] = { "<C-w>v", "split vertically" },
-    -- lazygit
-    -- ["<leader>lg"] = { "<cmd>lazygit<cr>","open lazygit" },
   },
 }
 
@@ -46,42 +41,16 @@ M.nvterm = {
   },
 }
 
---M.harpoon = {
--- n = {
---   ["<leader>a"] = {
---     mark.add_file,
---   },
---
---   ["<c-e>"] = {
---     ui.toggle_quick_menu,
---   },
---
---   ["<c-s>"] = {
---     function()
---       ui.nav_file(1)
---     end,
---   },
---
---   ["<c-d>"] = {
---     function()
---       ui.nav_file(2)
---     end,
---   },
---
---   ["<c-f>"] = {
---     function()
---       ui.nav_file(3)
---     end,
---   },
---
---   ["<c-g>"] = {
---     function()
---       ui.nav_file(4)
---     end,
---   },
--- },
---}
-
 -- -- more keybinds!
+M.nvimtree = {
+  plugin = true,
+  n = {
+    -- toggle
+    ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
+
+    -- toggle
+    ["<C-n>"] = { "<nop>", "toggle nvimtree" },
+  },
+}
 
 return M
