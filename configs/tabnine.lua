f4bab7d5f3da007ff3d65b0b39b6f208bf42b88e
ceaser008 <radiusrazzat111@gmail.com>
2023-04-16 10:08:12 +0545

    created: tabnine config

---
+++ configs/tabnine.lua
@@ -0,0 +1,16 @@
+local present, tabnine = pcall(require, "tabnine")
+
+if not present then
+  return
+end
+
+local option = {
+  disable_auto_comment = false,
+  accept_keymap = "<Tab>",
+  dismiss_keymap = "<C-]>",
+  debounce_ms = 800,
+  suggestion_color = { gui = "#808080", cterm = 244 },
+  exclude_filetypes = { "TelescopePrompt" },
+}
+
+tabnine.setup(option)
