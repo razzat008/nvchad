2b5760d17e1575ab4c505b6f7e13b3abfb0376d8
ceaser008 <radiusrazzat111@gmail.com>
2023-09-19 07:23:06 +0545

    changed theme

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "tomorrow_night",
-  theme_toggle = { "tomorrow_night", "tomorrow_night" },
+  theme = "gatekeeper",
+  theme_toggle = { "gatekeeper", "gatekeeper" },
   transparency = true,
 
   hl_override = highlights.override,
