be704ee5f49cb01cf63badae468e1f8122cee302
Razzat <radiusrazzat111@gmail.com>
2024-10-04 11:33:59 +0545

    themes

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "gruvbox",
-  theme_toggle = { "gruvbox", "gruvbox" },
+  theme = "doomchad",
+  theme_toggle = { "doomchad", "doomchad" },
   transparency = true,
 
   hl_override = highlights.override,
